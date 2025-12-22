fdf2a0c03e9fa1c0583837a7c6dcbd7802035b37
madhavareddyrb <madhavareddyrb@gmail.com>
2025-12-22 18:53:57 +0530

    configured app level urls,and started doing add task functionality

--- CURD-- Todo App/todo_main/todo/admin.py
+++ CURD-- Todo App/todo_main/todo/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Task
 # Register your models here.
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+  list_display = ( 'task', 'is_completed', 'updated_at')
+
+admin.site.register(Task,TaskAdmin)
--- CURD-- Todo App/todo_main/todo_main/views.py
+++ CURD-- Todo App/todo_main/todo_main/views.py
@@ -3,7 +3,9 @@ from todo.models import Task
 
 def home(request):
   tasks = Task.objects.filter(is_completed = False)
+  completed_tasks = Task.objects.filter(is_completed = True)
   context = {
     'tasks': tasks,
+    'completed_tasks' : completed_tasks
   }
   return render(request, 'home.html', context)
