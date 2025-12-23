fdbc72d4e542d7173178f883bdee69f444b831c2
madhavareddyrb <madhavareddyrb@gmail.com>
2025-12-23 21:10:54 +0530

    TODO Completed all CURD operations, post methodes,get,URLs

--- CURD-- Todo App/todo_main/todo/views.py
+++ CURD-- Todo App/todo_main/todo/views.py
@@ -15,6 +15,27 @@ def mark_as_done(request, pk):
 
   return redirect("home")
 
+def mark_as_undone(request, pk):
+  task =  get_object_or_404(Task, pk = pk)
+  task.is_completed = False
+  task.save()
+  return redirect("home")
+
+def edit_task(request, pk):
+  get_task = get_object_or_404(Task, pk = pk)
+  if request.method == "POST":
+    new_task = request.POST['task']
+    get_task.task = new_task
+    get_task.save()
+    return redirect("home")
+  else:
+    context = {
+      'get_task' : get_task,
+    }
+  
+  return render(request, 'edit_task.html',context)
+
+
 def delete_task(request,pk):
   task = get_object_or_404(Task, pk=pk)
   task = task.delete()
